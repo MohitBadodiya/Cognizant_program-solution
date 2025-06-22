bbce5d62d1c192c38a350a58b43cfbcdee0ccd4d
Mohit Badodiya <116389418+MohitBadodiya@users.noreply.github.com>
2025-06-22 19:56:09 +0530

    Update Forecast.java

--- week1/Data Structure and Algorithm/Financial Forecasting/Forecast.java
+++ week1/Data Structure and Algorithm/Financial Forecasting/Forecast.java
@@ -1,1 +1,19 @@
+public class Forecast {
+
+    // Recursive method to calculate future value
+    static double predict(double amount, double growth, int years) {
+        if (years == 0)  // Stop when years become 0
+            return amount;
+        return predict(amount * (1 + growth), growth, years - 1);
+    }
+
+    public static void main(String[] args) {
+        double current = 10000;   // Starting amount
+        double rate = 0.10;       // 10% growth rate
+        int time = 5;             // Number of years
+
+        double futureValue = predict(current, rate, time);
+        System.out.println("Value after " + time + " years: ₹" + futureValue);
+    }
+}
 
