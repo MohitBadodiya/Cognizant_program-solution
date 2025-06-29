8156de475ca6790b6390419f2e5986c16170e693
Mohit Badodiya <116389418+MohitBadodiya@users.noreply.github.com>
2025-06-29 13:55:05 +0530

    Update code.sql

--- week2/PL-SQL Programming/1.Control Structure/Scenario1/code.sql
+++ week2/PL-SQL Programming/1.Control Structure/Scenario1/code.sql
@@ -1,1 +1,28 @@
+BEGIN
+    FOR res IN (
+        SELECT c.name, c.customerID, l.loanId, l.interestRate
+        FROM loans l
+        JOIN customers c ON l.customerid = c.customerid
+        WHERE TRUNC(MONTHS_BETWEEN(SYSDATE, c.dob) / 12) > 60
+    ) LOOP
+        UPDATE loans
+        SET interestRate = res.interestRate - 1
+        WHERE loanID = res.loanID;
+    END LOOP;
 
+    COMMIT;
+    DBMS_OUTPUT.PUT_LINE('Interest rates updated for customers above 60.');
+END;
+/
+
+
+SELECT
+    loans.*,
+    loans.loanid       AS loanid1,
+    loans.customerid   AS customerid1,
+    loans.loanamount   AS loanamount1,
+    loans.interestrate AS interestrate1,
+    loans.startdate    AS startdate1,
+    loans.enddate      AS enddate1
+FROM
+    loans
