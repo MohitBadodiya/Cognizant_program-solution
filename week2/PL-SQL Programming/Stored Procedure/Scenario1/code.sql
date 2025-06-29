f06c1e9aabdd8d7a44be029001d7bd7f8b26b88e
Mohit Badodiya <116389418+MohitBadodiya@users.noreply.github.com>
2025-06-29 16:33:45 +0530

    Update code.sql

--- week2/PL-SQL Programming/Stored Procedure/Scenario1/code.sql
+++ week2/PL-SQL Programming/Stored Procedure/Scenario1/code.sql
@@ -1,1 +1,24 @@
+CREATE OR REPLACE PROCEDURE ProcessMonthlyInterest
+AS
+BEGIN
+    -- Loop through all Savings accounts
+    FOR res IN (
+        SELECT AccountID, Balance 
+        FROM Accounts 
+        WHERE AccountType = 'Savings'
+    ) LOOP
+        -- Update balance by adding 1% interest
+        UPDATE Accounts
+        SET Balance = Balance + (Balance * 0.01)
+        WHERE AccountID = res.AccountID;
+    END LOOP;
+
+    -- Commit the changes
+    COMMIT;
+
+    -- Display message
+    DBMS_OUTPUT.PUT_LINE('Monthly interest applied to all Savings accounts.');
+END;
+/
+EXEC ProcessMonthlyInterest;
 
