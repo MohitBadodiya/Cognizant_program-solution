4f2c6170efca59e1454fba3d9aa7ceff3d5df0e2
Mohit Badodiya <116389418+MohitBadodiya@users.noreply.github.com>
2025-06-29 17:20:37 +0530

    Update AAAExampleTest.java

--- week2/TESTING /src/test/java/AAAExampleTest.java
+++ week2/TESTING /src/test/java/AAAExampleTest.java
@@ -1,1 +1,30 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
+public class AAAExampleTest {
+
+    private com.myorg.dn4.Calculator calculator;
+
+    @Before
+    public void setUp() {
+        // ARRANGE: Initialize Calculator instance
+        calculator = new com.myorg.dn4.Calculator();
+    }
+
+    @After
+    public void cleanUp() {
+        // TEARDOWN: Clear reference
+        calculator = null;
+    }
+
+    @Test
+    public void testAddUsingAAA() {
+        // ACT: Perform addition
+        int actualResult = calculator.add(10, 20);
+
+        // ASSERT: Verify result
+        assertEquals(30, actualResult);
+    }
+}
