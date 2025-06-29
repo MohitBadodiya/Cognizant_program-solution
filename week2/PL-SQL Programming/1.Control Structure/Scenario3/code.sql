5f7d32765f6501646915d066e807939dd4447765
Mohit Badodiya <116389418+MohitBadodiya@users.noreply.github.com>
2025-06-29 15:11:59 +0530

    Update code.sql

--- week2/PL-SQL Programming/1.Control Structure/Scenario3/code.sql
+++ week2/PL-SQL Programming/1.Control Structure/Scenario3/code.sql
@@ -1,1 +1,16 @@
+BEGIN
+    FOR res IN (
+        SELECT c.Name, l.LoanAmount, l.EndDate
+        FROM Loans l
+        JOIN Customers c ON l.CustomerID = c.CustomerID
+        WHERE l.EndDate BETWEEN SYSDATE AND SYSDATE + 30
+    ) LOOP
+        DBMS_OUTPUT.PUT_LINE(
+            'Reminder: ' || res.Name || 
+            ', your loan of $' || res.LoanAmount || 
+            ' is due on ' || TO_CHAR(res.EndDate, 'DD-MON-YYYY')
+        );
+    END LOOP;
+END;
+/
 
