3693a24bd1fb55bb6d98f751d92f6a74b7fcbb8f
Mohit Badodiya <116389418+MohitBadodiya@users.noreply.github.com>
2025-06-29 14:31:37 +0530

    Update code.sql

--- week2/PL-SQL Programming/1.Control Structure/scenario2/code.sql
+++ week2/PL-SQL Programming/1.Control Structure/scenario2/code.sql
@@ -1,1 +1,24 @@
+ALTER TABLE Customers ADD IsVIP VARCHAR2(5);
+
+
+BEGIN
+    -- Loop through customers with balance > 10000
+    FOR res IN (
+        SELECT CustomerID, Name, Balance 
+        FROM Customers 
+        WHERE Balance > 10000
+    ) LOOP
+        -- Update IsVIP flag to 'TRUE'
+        UPDATE Customers
+        SET IsVIP = 'TRUE'
+        WHERE CustomerID = res.CustomerID;
+    END LOOP;
+
+    COMMIT;
+
+    DBMS_OUTPUT.PUT_LINE('VIP status updated for customers with balance over $10,000.');
+END;
+/
+
+SELECT CustomerID, Name, Balance, IsVIP FROM Customers;
 
