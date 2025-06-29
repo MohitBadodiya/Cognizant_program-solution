c07e5e8656f73cb7ae0ec0b88d6ad9fdc65f6230
Mohit Badodiya <116389418+MohitBadodiya@users.noreply.github.com>
2025-06-29 17:03:53 +0530

    Update LoggingExample.java

--- week2/TESTING /src/main/java/LoggingExample.java
+++ week2/TESTING /src/main/java/LoggingExample.java
@@ -7,7 +7,7 @@ public class LoggingExample {
             LoggerFactory.getLogger(LoggingExample.class);
 
     public static void main(String[] args) {
-        logger.error("This is an error message");
-        logger.warn ("This is a warning message");
+        logger.error("This is an error display");
+        logger.warn ("This is a warning display");
     }
 }
