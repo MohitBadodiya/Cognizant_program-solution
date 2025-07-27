5b8e1b0ef0a9498facae76cb12a0683cb5ddfca1
Mohit Badodiya <116389418+MohitBadodiya@users.noreply.github.com>
2025-07-27 22:43:33 +0530

    Add files via upload

---
+++ week6/scorecalculatorapp/src/App.js
@@ -0,0 +1,14 @@
+import { CalculateScore } from "./Components/CalculateScore";
+
+function App() {
+  return (
+    <div>
+      <CalculateScore Name ={"Steeve"}
+      School={"Red Rose School"}
+      total={345}
+      goal={4}
+       />
+    </div>
+  );
+}
+export default App;
